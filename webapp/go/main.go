934ac549157fc5f677114de6256a2bce1c32677f
ktny <katsunori.t10@gmail.com>
2020-09-12 19:03:19 +0900

    revert id

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -525,7 +525,8 @@ func searchChairs(c echo.Context) error {
 	searchQuery := "SELECT * FROM chair WHERE "
 	countQuery := "SELECT COUNT(*) FROM chair WHERE "
 	searchCondition := strings.Join(conditions, " AND ")
-	limitOffset := " ORDER BY popularity DESC LIMIT ? OFFSET ?"
+	limitOffset := " ORDER BY popularity DESC, id ASC LIMIT ? OFFSET ?"
+	// limitOffset := " ORDER BY popularity DESC LIMIT ? OFFSET ?"
 
 	var res ChairSearchResponse
 
