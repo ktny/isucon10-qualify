dd551bab4a5efe6c6a915d3f9185b28bdfd84376
ktny <katsunori.t10@gmail.com>
2020-09-17 23:37:08 +0900

    update deploy_db

--- deploy_db.sh
+++ deploy_db.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -x
 
+# 事前に/etc, /logディレクトリの権限を緩めておく
+
 echo "start deploy db ${USER}"
 
 for server in isu02; do
     ssh -tq $server "sudo systemctl stop mysql"
-    # scp isu02/my.cnf $server:/etc/mysql/my.cnf
-    ssh -tq $server "sudo rm -f /var/log/mysql/*.log"
+    scp isu02/my.cnf $server:/etc/mysql/my.cnf
+    ssh -tq $server "sudo rm -f /var/log/mysql/*"
     ssh -tq $server "sudo mysql -uisucon -pisucon -e'flush logs;' isuumo"
     ssh -tq $server "sudo systemctl start mysql"
 done
