b30c17a09673985c14c137561b3f7c5ace9347f9
ktny <katsunori.t10@gmail.com>
2020-09-20 18:26:38 +0900

    use isu02 for estate, isu03 for chair

---
+++ webapp/mysql/db/0_Schema_estate.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS isuumo;
+CREATE DATABASE isuumo;
+
+DROP TABLE IF EXISTS isuumo.estate;
+DROP TABLE IF EXISTS isuumo.chair;
+
+CREATE TABLE isuumo.estate
+(
+    id          INTEGER             NOT NULL PRIMARY KEY,
+    name        VARCHAR(64)         NOT NULL,
+    description VARCHAR(512)        NOT NULL,
+    thumbnail   VARCHAR(128)        NOT NULL,
+    address     VARCHAR(128)        NOT NULL,
+    latitude    DOUBLE PRECISION    NOT NULL,
+    longitude   DOUBLE PRECISION    NOT NULL,
+    rent        INTEGER             NOT NULL,
+    door_height INTEGER             NOT NULL,
+    door_width  INTEGER             NOT NULL,
+    features    VARCHAR(64)         NOT NULL,
+    popularity  INTEGER             NOT NULL,
+    popularity_desc INTEGER AS (-popularity) NOT NULL,
+    INDEX       idx_rent (rent),
+    INDEX       idx_popularity (popularity),
+    INDEX       idx_popularity_desc (popularity_desc),
+    INDEX       idx_lat_long (latitude, longitude),
+    INDEX       idx_door_width_height (door_width, door_height)
+);
